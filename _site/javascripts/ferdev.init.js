309d614b0e87c79ca3a7559f450e7ae16a90912d
Fernando Espinosa <fer@ferdev.com>
2010-06-30 17:14:20 +0200

    Adds javascript to simulate position:fixed on navigation bar

--- _site/javascripts/ferdev.init.js
+++ _site/javascripts/ferdev.init.js
@@ -21,7 +21,16 @@ var ferdev = function(){
       var 
         destination_id = $(this).attr('href'),
         scrollTo       = $(destination_id).offset().top;
-      $('html, body').animate({scrollTop: scrollTo}, 2000, 'easeInOutExpo');
+      $('html,body').animate({scrollTop: scrollTo}, 2000, 'easeInOutExpo');
+    });
+    var 
+      navigation = $('nav'),
+      navigation_top = parseInt(navigation.css('top'));
+    $(document).scroll(function(evt){
+      clearTimeout(this.timeout);
+      this.timeout = setTimeout(function(){
+        navigation.animate({'top': $(this).scrollTop() + navigation_top}, 1000, 'easeInOutExpo');
+      }, 100);      
     });
   };  
   
