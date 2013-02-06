8bbc9bad81446a323d6228fbc6f37cf7629ac76c
Fernando Espinosa <fer@ferdev.com>
2013-02-06 01:04:19 +0100

    Uses ENV vars for app_config if there isn't a yml file

--- app_config.rb
+++ app_config.rb
@@ -1,1 +1,3 @@
-APP_CONFIG = YAML.load_file(File.expand_path('../app_config.yml', __FILE__))
+APP_CONFIG = ENV
+config_file_path = File.expand_path('../app_config.yml', __FILE__)
+APP_CONFIG = YAML.load_file(config_file_path) if File.exists?(config_file_path)
